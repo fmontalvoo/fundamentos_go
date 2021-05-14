2bd337a4c53e5470f22556196aaef50ca66f2893
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-14 10:49:56 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ introduccion/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// go build main.go -> Compila el codigo y genera un ejecutable.
+// go run main.go -> Ejecuta directamente el codigo.
+
+func main() {
+	fmt.Println("¡Hola Mundo!")
+}
