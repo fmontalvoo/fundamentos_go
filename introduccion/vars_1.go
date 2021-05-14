0977ae42394c392d3f75e60bab5b697790dfd48b
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-14 11:11:32 -0500

    Declaracion de multiples variables.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ introduccion/vars_1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var nombre, apellido string = "Frank", "Montalvo"
+	// var nombre, apellido = "Frank", "Montalvo"
+	nombre, apellido := "Frank", "Montalvo"
+	edad, altura := 25, 1.75
+
+	fmt.Println(nombre, apellido)
+	fmt.Println(edad, altura)
+}
