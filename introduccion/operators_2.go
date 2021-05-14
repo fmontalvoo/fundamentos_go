553c04d41d7706d4402bd4669b9f1b1a9e2a7f70
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-14 11:37:38 -0500

    Operadores logicos.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- introduccion/operators_2.go
+++ introduccion/operators_2.go
@@ -5,6 +5,17 @@ import "fmt"
 // Operadores logicos.
 func main() {
 
-	
+	 a := 10
+	 b := 5
+	 c := 5
+	 d := -1
+
+	 and := a > b && b > c
+	 or := a != d || c > b
+	 negation := !(a > b && (b != d || d > c))
+
+	 fmt.Println("Operador And:", and)
+	 fmt.Println("Operador Or:", or)
+	 fmt.Println("Operador de negacion:", negation, !negation)
 
 }
