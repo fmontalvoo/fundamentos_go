7b39b2883c6ede944eff0f8c07af42ca4800c6cc
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-18 10:53:31 -0500

    Corrigo error en condicion.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- control_de_flujo/conditionals_02.go
+++ control_de_flujo/conditionals_02.go
@@ -22,9 +22,9 @@ func main() {
 
 	fmt.Println("+----------------+")
 
-	if a > b && b > c {
+	if a > b && a > c {
 		fmt.Println("A es el mayor")
-	} else if b > a && a > c {
+	} else if b > a && b > c {
 		fmt.Println("B es el mayor")
 	} else {
 		fmt.Println("C es el mayor")
